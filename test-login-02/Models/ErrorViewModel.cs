2a4f80e5a3d2fe41dc6c11d1943fd6ae405b5341
VBALBOA <VBALBOA@FG030-GRP>
2025-04-01 17:22:06 -0500

    Agregar archivos de proyecto.

---
+++ test-login-02/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace test_login_02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
