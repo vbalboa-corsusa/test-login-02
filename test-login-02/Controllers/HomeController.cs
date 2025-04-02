3c5a68c67ef6a9559523568391414868bf3dcd7b
VBALBOA <VBALBOA@FG030-GRP>
2025-04-02 10:03:38 -0500

    feat: llamado y muestra de conexion con bitrix api

--- test-login-02/Controllers/HomeController.cs
+++ test-login-02/Controllers/HomeController.cs
@@ -1,32 +1,57 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using test_login_02.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq; //consulta, analisis y manipulacion de datos JSON. LINQ(Idioma Consulta Integrada)
 
 namespace test_login_02.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private static readonly HttpClient _client = new HttpClient(); //Cliente http
+        public ActionResult Index()
         {
-            _logger = logger;
+            return View(); //Retorno de la vista
         }
 
-        public IActionResult Index()
+        [HttpPost]
+        public async Task<ActionResult> Login(string email, string password)
         {
-            return View();
-        }
+            string url = "https://corsusaint.bitrix24.com/rest/5728/ooyssbe5leg0hl3t/user.current";
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            try
+            {
+                var response = await _client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync(); //Serializa contenido http en una cadena
+                    //ViewBag.Message = "Acceso correcto: " + content; //ViewBag maneja dinamicamente los datos a la vista
+                    var jsonResponse = JObject.Parse(content);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+                    if (jsonResponse["result"] != null)
+                    {
+                        var user = jsonResponse["result"];
+                        string userEmail = user["EMAIL"]?.ToString() ?? "Incorrecto"; //?? se usa para gestionar valores nulos
+
+                        ViewBag.Message = $"Acceso correcto. Usuario: {userEmail}";
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Error al acceder a la API.";
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Error al acceder a la API.";
+                }
+            }
+            catch
+            {
+                ViewBag.Message = "Error al acceder a la API.";
+            }
+
+            return View("Index");
         }
     }
 }
