b869aecef901ba00f9b550e0dc326a7a5aa3e5f8
VBALBOA <VBALBOA@FG030-GRP>
2025-04-02 17:17:44 -0500

    docs: comments to apply

--- test-login-03/Models/TokenResult.cs
+++ test-login-03/Models/TokenResult.cs
@@ -2,8 +2,9 @@
 {
     public class TokenResult
     {
-        public string access_token { get; set; }
-        public string token_type { get; set; }
+        //revisar la creación de propiedad Required
+        public string client_id { get; set; }
+        public string client_secret { get; set; }
         //public string scope { get; set; }
         //public string expires_in { get; set; }
         //public string refresh_token { get; set; }
