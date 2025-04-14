434e4c401a3cde273ac5f1fcd1cd9fd25cfe014d
vbalboa-corsusa <vbalboa@corsusa.com>
2025-04-14 17:10:08 -0500

    feat: add files to create dashboard

---
+++ test-login-02/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace test_bitrix24_api.Models
+{
+  public class Contact
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Company { get; set; }
+    public string IsDeleted { get; set; }
+  }
+}
